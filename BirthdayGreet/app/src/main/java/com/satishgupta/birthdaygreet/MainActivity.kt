fe05b9fc2c5a1bd00bd488a40195394e9aae1e2c
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2024-04-05 18:32:32 +0530

    Button Clicks | Toast Message
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- BirthdayGreet/app/src/main/java/com/satishgupta/birthdaygreet/MainActivity.kt
+++ BirthdayGreet/app/src/main/java/com/satishgupta/birthdaygreet/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.satishgupta.birthdaygreet
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun createBirthdayCard(view: View) {
+        val name = findViewById<EditText>(R.id.nameInput).text.toString();
+        Toast.makeText(this, "Button was clicked. Name: $name", Toast.LENGTH_LONG).show()
     }
 }
